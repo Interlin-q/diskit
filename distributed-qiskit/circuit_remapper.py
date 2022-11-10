beba7a1cc27e0fe7788b2ad8a8ea8dc4763295a5
Stephen Diadamo <stephen.diadamo@gmail.com>
2022-11-10 13:54:48 +0000

    Adding some placeholder classes

---
+++ distributed-qiskit/circuit_remapper.py
@@ -0,0 +1,16 @@
+'''
+The circuit remapper logic.
+'''
+class CircuitRemapper:
+    '''
+    The circuit remapper class for remapping a ciruit to a topology.
+    '''
+    def __init__(self, circuit, topology):
+        self.circuit = circuit
+        self.topology = topology
+
+    def remap_circuit(self):
+        '''
+        Remap the circuit for the topology.
+        '''
+        return None
---
+++ distributed-qiskit/topology.py
@@ -0,0 +1,13 @@
+'''Topology class for mapping distributed quantum computers.'''
+
+class Topology:
+    '''
+    Topology class for a distributed architecture.
+    '''
+    def __init__(self):
+        pass
+
+    def num_qubits(self):
+        '''Return the total number of qubits in the topology'''
+        return 0
+        
