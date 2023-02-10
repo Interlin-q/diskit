c2b36ef03617f4996e632b293d0630de4e01c543
Stephen DiAdamo <stephen.diadamo@gmail.com>
2023-02-10 05:32:05 +0100

    Adding qiskit requirement to install

--- setup.py
+++ setup.py
@@ -5,19 +5,23 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="diskit",
-    version="0.0.1",
+    version="0.0.1.post1",
+    license='Apache',
+    keywords=['quantum', 'distributed', 'simulator', 'qiskit', 'diskit'],
     author="Anuranan Das, Stephen DiAdamo",
     author_email="stephen.diadamo@gmail.com",
     description="Distributed circuit remapping for Qiskit",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Interlin-q/diskit",
+    install_requires=[
+        'qiskit'
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "diskit"},
-    packages=setuptools.find_packages(where="src"),
+    packages=setuptools.find_packages(),
     python_requires=">=3.6"
 )
